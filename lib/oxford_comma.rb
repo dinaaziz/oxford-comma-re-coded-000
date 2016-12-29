348a20d4313f239e15f3d15ca9c44d956742416f
dina aziz <dina.aziz100@yahoo.com>
2016-12-29 00:19:53 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+if array.size==1
+array.join
+elsif
+array.size==2
+array.join(" and ")
+elsif
+  array.size==3
+  last=array.pop
+    array.join(", ")<<", and "<<last
+else
+  array.size>3
+  last=array.pop
+    array.join(", ")<<", and "<<last
 end
+end
+ #oxford_comma"["fiddleheads","okra","kohlrabi"]"
